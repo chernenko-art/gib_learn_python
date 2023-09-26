4d50df1a88ffe417a9b800c846bcc844f6c8927f
Artem Chernenko <a.chernenko@facct.ru>
2023-09-26 13:28:02 +0700

    Lesson 8.1. First method parts complete

--- lesson_8_1.py
+++ lesson_8_1.py
@@ -22,33 +22,70 @@
 # Само собой, нужно спроектировать класс так, чтобы было как можно меньше дублирования в коде."
 
 
-class UserDs:
-    def __init__(self, id, name, fname, county, age, is_teen):
-        self.id = id
-        self.name = name
-        self.fname = fname
-        self.country = county
-        self.age = age
-        self.is_teen = is_teen
-        self.db = "users_ds.json"
+import json
 
-    def get_all_users_from_country(self, country):
-        pass
+
+def _get_db_list(db_file_path):
+    with open(db_file_path, "r") as f:
+        return json.load(f)
+
+
+class UserDs:
+    def __init__(self, db_path):
+        self.db_path = _get_db_list(db_path)
+        self.id = 'id'
+        self.name = 'name'
+        self.fname = 'fname'
+        self.county = 'county'
+        self.age = 'age'
+        self.is_teen = 'is_teen'
+
+    def _db_searching(self, key, value, older=None, younger=None):
+        new_array = []
+        for i in self.db_path:
+            if older:
+                if i[key] > value:
+                    new_array.append(i)
+            elif younger:
+                if i[key] <= value:
+                    new_array.append(i)
+            else:
+                if i[key] == value:
+                    new_array.append(i)
+        return new_array
+
+    def get_all_users_from_county(self, county):
+        return self._db_searching(key=self.county, value=county)
 
     def get_all_users_equal_age(self, age):
-        pass
+        return self._db_searching(key=self.age, value=age)
 
     def get_all_users_older_age(self, age):
-        pass
+        return self._db_searching(key=self.age, value=age, older=True)
 
     def get_all_users_younger_or_equal_age(self, age):
-        pass
+        return self._db_searching(key=self.age, value=age, younger=True)
 
-    def get_all_adults(self, age):
-        pass
+    def get_all_adults(self, is_teen=False):
+        return self._db_searching(key=self.is_teen, value=is_teen)
 
-    def get_all_teens(self, age):
-        pass
+    def get_all_teens(self, is_teen=True):
+        return self._db_searching(key=self.is_teen, value=is_teen)
 
     def get_collisions(self):
         pass
+
+
+def main():
+    user_ds = UserDs(db_path="users_ds.json")
+    all_users_from_county = user_ds.get_all_users_from_county(county="Russia")
+    all_users_equal_age = user_ds.get_all_users_equal_age(age=29)
+    all_users_older_age = user_ds.get_all_users_older_age(age=29)
+    all_users_younger_or_equal_age = user_ds.get_all_users_younger_or_equal_age(age=29)
+    all_adults = user_ds.get_all_adults()
+    all_teens = user_ds.get_all_teens()
+    print(all_adults)
+
+
+if __name__ == "__main__":
+    main()
