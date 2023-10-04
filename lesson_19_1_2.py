90d3ff11275a04ec7556742a71c0616a1b5188bb
Artem Chernenko <a.chernenko@facct.ru>
2023-10-04 09:48:18 +0700

    Lesson 19_1, 19_2. Done

--- lesson_19_1_2.py
+++ lesson_19_1_2.py
@@ -3,6 +3,7 @@
 # Иначе вернуть FALSE"
 
 
+
 class SearchNum:
 
     @staticmethod
@@ -12,9 +13,21 @@ class SearchNum:
         else:
             return False
 
+# 19_2==============
+# У вас чуть сложнее. Функция получает два массива чисел. Она должна вернуть TRUE в случае, если каждая цифра из второго массива встречается
+# в первом массиве хотя бы раз. Иначе FALSE.
+
+    @staticmethod
+    def search_matching_num_two_array(array_1, array_2):
+        result = False
+        for i in array_2:
+            result = True if i in array_1 else False
+        return result
+
 
 def main():
     print(SearchNum.search_num([1, 2, 40, 555, 13, 49, 53, 41, 324567]))
+    print(SearchNum.search_matching_num_two_array([1, 2, 40, 555, 13, 49, 53, 41, 324567], [40, 13, 1]))
 
 
 if __name__ == '__main__':
