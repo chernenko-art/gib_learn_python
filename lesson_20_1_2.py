74a0353784d86ffb0ac87ba0c7bcc002c5b72440
Artem Chernenko <a.chernenko@facct.ru>
2023-10-06 13:47:15 +0700

    Lesson 20_1, 20_2. Done

--- lesson_20_1_2.py
+++ lesson_20_1_2.py
@@ -3,11 +3,15 @@
 # Каждая клеточка может иметь одно из трех значений - зеленый (З), красный (К) или синий (С)
 # Ваша задача - понять, есть ли путь из клетки 1х1 к клетке 3х3 так, чтобы мы не шли по диагонали и чтобы за всю дорогу ни разу не сменили цвет.
 # То есть мы можем переступать на соседнюю клеточку только того же цвета, с которого начала на 1х1"
+# === 20_2
+# У вас все то же самое, только поле может быть любого размера, необязательно 3х3. И у вас можно идти по диагонали.
 
 class PathSearch:
 
     def __init__(self, canvas):
         self.canvas = canvas
+        self. x_len = len(self.canvas[0]) - 1
+        self.y_len = len(self.canvas) - 1
         self.start_pos_letter = self.canvas[0][0]
 
     def print_canvas(self):
@@ -27,16 +31,19 @@ class PathSearch:
             return False
 
     def path_search(self):
+        """Реализовал движение не сверху вниз, а наоборот от угловой нижней клетки к верхней"""
+
         step_up = (0, -1)
         step_left = (-1, 0)
-        match_array = [[2, 2]]
+        step_diag = (-1, -1)
+        match_array = [[self.x_len, self.y_len]]
 
         while match_array:
             current_x,  current_y = match_array.pop()
             if current_x == 0 and current_y == 0:
                 return True
 
-            for x, y in [step_up, step_left]:
+            for x, y in [step_up, step_left, step_diag]:
                 new_pos = self._try_step(x, y, current_x,  current_y)
                 if new_pos:
                     match_array.append(new_pos)
@@ -45,7 +52,11 @@ class PathSearch:
 
 
 def main():
-    searcher = PathSearch([("К", "К", "З"), ("З", "К", "К"), ("С", "К", "К")])
+    searcher = PathSearch([("К", "К", "К", "К"),
+                           ("К", "К", "З", "З"),
+                           ("К", "С", "К", "К"),
+                           ("К", "К", "С", "К")
+                           ])
     searcher.print_canvas()
     print(searcher.path_search())
 
