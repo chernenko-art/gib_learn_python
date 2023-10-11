65fcf8f8b0658e3cccc13f9467dc40f75cfa3779
Artem Chernenko <a.chernenko@facct.ru>
2023-10-11 20:48:57 +0700

    Refactor lesson 8.2

--- lesson_8_2.py
+++ lesson_8_2.py
@@ -16,65 +16,62 @@ from data.files_path import lesson8
 
 
 class UserDs:
-    def __init__(self):
-        self.db_file_path = lesson8["db_file_path"]
-        self.new_db_path = lesson8["new_db_path"]
-        self.county_adult_age_file_path = lesson8["county_adult_age_file_path"]
-        self.id = 'id'
-        self.name = 'name'
-        self.fname = 'fname'
-        self.county = 'county'
-        self.age = 'age'
-        self.is_teen = 'is_teen'
 
-    def _get_county_adult_age(self):
-        with open(self.county_adult_age_file_path, "r") as f:
+    @staticmethod
+    def _get_county_adult_age():
+        with open(lesson8["county_adult_age_file_path"], "r") as f:
             return json.load(f)
 
-    def _get_db_list(self):
-        with open(self.db_file_path, "r") as f:
+    @staticmethod
+    def _get_users():
+        with open(lesson8["db_file_path"], "r") as f:
             return json.load(f)
 
-    def _save_db_to_new_file(self, data):
-        with open(self.new_db_path, "w+") as f:
+    @staticmethod
+    def _save_db_to_new_file(data):
+        with open(lesson8["new_db_path"], "w+") as f:
             json.dump(data, f, indent=4)
 
-    def correct_all_records(self):
-        db = self._get_db_list()
+    @staticmethod
+    def _correct(user, age_list):
+        if user['is_teen'] and age_list[user['county']] < user['age']:
+            user['is_teen'] = False
+            print(f"corrected_id: {user['id']}")
+        elif age_list[user['county']] > user['age']:
+            user['is_teen'] = True
+            print(f"corrected_id: {user['id']}")
+
+    def correct_all_records(self, id=None):
+        users = self._get_users()
         county_adult_age_json = self._get_county_adult_age()
-        for i in db:
-            if i[self.is_teen]:
-                if county_adult_age_json[i[self.county]] < i[self.age]:
-                    i[self.is_teen] = False
-                    print(f"corrected_id: {i[self.id]}")
-            else:
-                if county_adult_age_json[i[self.county]] > i[self.age]:
-                    i[self.is_teen] = True
-                    print(f"corrected_id: {i[self.id]}")
-        self._save_db_to_new_file(db)
+
+        for user in users:
+            self._correct(user, county_adult_age_json)
+
+        self._save_db_to_new_file(users)
 
     def relocate_user(self, user_id, county):
-        db = self._get_db_list()
+        users = self._get_users()
         county_adult_age_json = self._get_county_adult_age()
+
         if county not in list(county_adult_age_json.keys()):
             raise Exception(f"Relocation is not impossible! County must be in: {list(county_adult_age_json.keys())}")
-        for i in db:
-            if i[self.id] == user_id:
-                if i[self.age] < county_adult_age_json[county] and not i[self.is_teen]:
-                    i[self.is_teen] = True
-                    print(f"is_teen param changed on True")
-                elif i[self.age] > county_adult_age_json[county] and i[self.is_teen]:
-                    i[self.is_teen] = False
-                    print(f"is_teen param changed on False")
-                i[self.county] = county
-                print(f"user with id:{user_id} {i[self.name]} {i[self.fname]} relocated to {county}")
-        self._save_db_to_new_file(db)
+
+        for user in users:
+            if user['id'] == user_id:
+                self._correct(user, county_adult_age_json)
+                user['county'] = county
+                print(f"user with id:{user_id} {user['name']} {user['fname']} relocated to {county}")
+
+        self._save_db_to_new_file(users)
 
     def append_county(self, new_county, teen_age):
         county_adult_age_json = self._get_county_adult_age()
         county_adult_age_json[new_county] = teen_age
-        with open(self.county_adult_age_file_path, 'r+') as f:
+
+        with open(lesson8["county_adult_age_file_path"], 'r+') as f:
             json.dump(county_adult_age_json, f, indent=4)
+
         print(f"County {new_county} appended")
 
 
